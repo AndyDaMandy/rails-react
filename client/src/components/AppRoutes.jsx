5669490552d9d3da227e33add838ee2d9697d328
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-09-10 10:17:00 -0400

    implemented jest testing

--- client/src/components/AppRoutes.jsx
+++ client/src/components/AppRoutes.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import PostsList from "../features/posts/PostsList.jsx";
 import PostDetails from "../features/posts/PostDetails.jsx";
 import NewPostForm from "../features/posts/NewPostForm.jsx";
